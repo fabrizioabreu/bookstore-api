900a593d304d7c4b937aa33ce26b641aa75f933c
Fabrizio Abreu <fabrizioabreu@live.com>
2021-04-20 17:35:15 -0300

    Excecao persobalizada para delete de Categoria

--- src/main/java/com/fabrizio/bookstore/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/fabrizio/bookstore/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.fabrizio.bookstore.service.exceptions.DataIntegrityViolationException;
 import com.fabrizio.bookstore.service.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -16,5 +17,11 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e, ServletRequest request) {
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request) {
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
