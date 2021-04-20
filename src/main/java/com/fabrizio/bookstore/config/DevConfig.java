6ff9a3e3708e36b6a1a0c0b5fdba3f4de5258623
Fabrizio Abreu <fabrizioabreu@live.com>
2021-04-20 11:43:36 -0300

    Criando perfil de DEV, Conectando ao MySQL

---
+++ src/main/java/com/fabrizio/bookstore/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.fabrizio.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.fabrizio.bookstore.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciaBaseDeDados() {
+		if (strategy.equals("create")) {
+			dbService.instanciaBaseDeDados();
+		}
+		return false;
+	}
+}
